b05fee57d239443776d8f1f6fce0b4de541812b1
bachtangthang <thang5217113@gmail.com>
2021-11-20 21:16:51 +0700

    Create node.js server

---
+++ backend/data.js
@@ -0,0 +1,102 @@
+const data = {
+	products: [
+		{
+			_id: '1',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p1.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 0.5,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '2',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p2.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 2.5,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '3',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p3.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 4.5,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '3',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p4.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 3.5,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '4',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p1.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 3.6,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '5',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p1.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 4,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '6',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p1.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 4,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		},
+		{
+			_id: '7',
+			name: 'Saiki',
+			category: 'comedy',
+			image: '/images/p1.jpg',
+			price: 5,
+			countInStock: 10,
+			brand: 'KimDong Comic',
+			rating: 4,
+			numReviews: 10,
+			description: 'story about a pyshic student'
+		}
+	]
+};
+
+export default data;
