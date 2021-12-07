d44c0538eb11bbe0d7394a224ecda7a21f9b51cb
bachtangthang <thang5217113@gmail.com>
2021-12-07 21:56:39 +0700

    connect to mongodb, create UserRoute, UserModel

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+	users: [
+		{
+			name: 'ThangBach',
+			email: 'admin@gmail.com',
+			password: bcrypt.hashSync('1234', 8),
+			isAdmin: true
+		},
+		{
+			name: 'gogo',
+			email: '123@gmail.com',
+			password: bcrypt.hashSync('1234', 8),
+			isAdmin: false
+		}
+	],
 	products: [
 		{
 			_id: '1',
